17f5810420dce55bd67484abc19f66a8a9aa36b7
Emzzy241 <emzzyoluwole@gmail.com>
2022-12-28 07:10:14 +0100

    committing source code for second written Test

--- src/triangle.js
+++ src/triangle.js
@@ -8,6 +8,6 @@ export default function Triangle(side1, side2, side3) {
       this.side3 = side3;
 }
 
-  // Triangle.prototype.checkType = function() {
-  //   return "I can't answer that yet!";
-  // }
+  Triangle.prototype.checkType = function() {
+    return "not a triangle";
+  }
