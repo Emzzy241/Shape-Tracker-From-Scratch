3919e8bd5c9502b9047343db000aaafba3518997
Emzzy241 <emzzyoluwole@gmail.com>
2022-12-28 07:49:36 +0100

    adding source code to for 3rd test

--- src/triangle.js
+++ src/triangle.js
@@ -1,13 +1,30 @@
 // code for business Logic 
 
 export default function Triangle(side1, side2, side3) {
-      this.side1 = side1;
-      // debugger;
-      //     bola();
-      this.side2 = side2;
-      this.side3 = side3;
+  this.side1 = side1;
+  // debugger;
+  //     bola();
+  this.side2 = side2;
+  this.side3 = side3;
 }
 
-  Triangle.prototype.checkType = function() {
+Triangle.prototype.checkType = function () {
+  // tests to determine scalene triangle already written and got a good fail, now its time to update source code to account for this
+  // conditional that determines whether its a scalene trianlgle or not
+  // this code checks to see if all three sides are different.... If they are, our method will return "scalene triangle"
+  // if((this.side1 !== this.side2) && (this.side2 !== this.side3) && (this.side2 !== this.side3)){
+  //   return "scalene triangle";
+  // }
+  // else{
+  //   return "not a triangle";
+  // }
+
+  // the if statement above works for our 1st and 3rd test but breaks the 2nd one, to avoid this now we refactor our code and add a conditional that actually checks whether the value of one side is greater than the sum of the other two sides
+  if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
   }
+  else if ((this.side1 !== this.side2) && (this.side1 !== this.side3) && (this.side2 !== this.side3)) {
+    return "scalene triangle";
+  }
+
+}
